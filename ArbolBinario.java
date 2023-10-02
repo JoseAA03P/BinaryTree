4427dd38c5d0d599d44b24b96b6c8d502bbf95d1
JoseAA03P <91348516+JoseAA03P@users.noreply.github.com>
2023-10-02 12:40:01 -0600

    Update ArbolBinario.java

--- ArbolBinario.java
+++ ArbolBinario.java
@@ -4,28 +4,28 @@ class ArbolBinario {
     public ArbolBinario(Node raiz) {
         this.raiz = raiz;
     }
-    
+    //rrecorrido preorden
     public void preorden(Node nodo) {
         if (nodo != null) {
             System.out.print(nodo.value() + " ");
-            preorden(nodo.left());
-            preorden(nodo.right());
+            preorden(nodo.left());//Si el nodo es null pasa por el nodo izquierdo
+            preorden(nodo.right());//Después pasa por el nodo derecho
         }
     }
-
+//recorrido inorden
     public void inorden(Node nodo) {
         if (nodo != null) {
-            inorden(nodo.left());
-            System.out.print(nodo.value() + " ");
-            inorden(nodo.right());
+            inorden(nodo.left()); //Si el nodo es null, pasa primero por el nodo izquierdo 
+            System.out.print(nodo.value() + " "); //Pone al nodo que estabamos trabajando
+            inorden(nodo.right()); //Pasa al nodo derecho
         }
     }
-
+//recorrido posorden
     public void posorden(Node nodo) {
         if (nodo != null) {
-            posorden(nodo.left());
-            posorden(nodo.right());
-            System.out.print(nodo.value() + " ");
+            posorden(nodo.left()); //Si el nodo es null, pasa al izquierdo
+            posorden(nodo.right()); //Luego al derecho
+            System.out.print(nodo.value() + " "); //Luego imprime el nodo actual
         }
     }
 }
